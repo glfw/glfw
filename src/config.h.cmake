b9769dea77786adb8397f7e4d9680bef0dbfc45a
Camilla Berglund <elmindreda@users.sourceforge.net>
2010-09-20 22:37:49 +0200

    Added Win32 configuration macros.

--- src/config.h.cmake
+++ src/config.h.cmake
@@ -12,5 +12,10 @@
 /* Define this to 1 if glXGetProcAddressEXT is available */
 #cmakedefine _GLFW_HAS_GLXGETPROCADDRESSEXT 1
 
+/* Define this to 1 to not load gdi32.dll dynamically */
+#cmakedefine _GLFW_NO_DLOAD_GDI32 1
+/* Define this to 1 to not load winmm.dll dynamically */
+#cmakedefine _GLFW_NO_DLOAD_WINMM 1
+
 #define _GLFW_VERSION_FULL "@GLFW_VERSION_FULL@"
 

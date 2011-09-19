4e9c34952cd79df299e5901b3282bb4df23eea0b
Camilla Berglund <elmindreda@elmindreda.org>
2011-09-19 18:46:20 +0200

    Pkg-config file generation work.

---
+++ src/libglfw.pc.cmake
@@ -0,0 +1,12 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=${prefix}
+includedir=${prefix}/include
+libdir=${exec_prefix}/lib
+
+Name: GLFW
+Description: A portable library for OpenGL, window and input
+Version: 3.0.0
+URL: http://www.glfw.org/
+Requires.private: gl x11 @GLFW_PKGLIBS@
+Libs: -L${libdir} -lglfw 
+Cflags: -I${includedir}

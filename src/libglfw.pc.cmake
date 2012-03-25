d743793e71ad3b0089a39f443c75391b832c3ee4
Camilla Berglund <elmindreda@elmindreda.org>
2012-03-25 14:51:56 +0200

    Pkg-config dependency generation fixes.

--- src/libglfw.pc.cmake
+++ src/libglfw.pc.cmake
@@ -7,6 +7,6 @@ Name: GLFW
 Description: A portable library for OpenGL, window and input
 Version: 3.0.0
 URL: http://www.glfw.org/
-Requires.private: gl x11 @GLFW_PKGLIBS@
+Requires.private: @GLFW_PKGLIBS@
 Libs: -L${libdir} -lglfw 
 Cflags: -I${includedir}

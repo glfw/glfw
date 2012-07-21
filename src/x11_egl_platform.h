e9c82a82b2c63cbbf98d4de06902dbdcdea71105
Camilla Berglund <elmindreda@elmindreda.org>
2012-07-21 23:51:44 +0200

    Added eglext.h header.

--- src/x11_egl_platform.h
+++ src/x11_egl_platform.h
@@ -33,6 +33,11 @@
 
 #include <EGL/egl.h>
 
+// This path may need to be changed if you build GLFW using your own setup
+// We ship and use our own copy of eglext.h since GLFW uses fairly new
+// extensions and not all operating systems come with an up-to-date version
+#include "../support/GL/eglext.h"
+
 // Do we have support for dlopen/dlsym?
 #if defined(_GLFW_HAS_DLOPEN)
  #include <dlfcn.h>

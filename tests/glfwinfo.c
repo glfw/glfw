6c86149bad13a5fe43683ff8af8737de8065934f
Camilla Berglund <elmindreda@elmindreda.org>
2012-07-31 23:14:31 +0200

    Improved output of OpenGL bitmasks.

--- tests/glfwinfo.c
+++ tests/glfwinfo.c
@@ -249,7 +249,7 @@ int main(int argc, char** argv)
     if (major >= 3)
     {
         glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
-        printf("OpenGL context flags:");
+        printf("OpenGL context flags (0x%08x):", flags);
 
         if (flags & GL_CONTEXT_FLAG_FORWARD_COMPATIBLE_BIT)
             puts(" forward-compatible");
@@ -263,7 +263,7 @@ int main(int argc, char** argv)
     if (major > 3 || (major == 3 && minor >= 2))
     {
         glGetIntegerv(GL_CONTEXT_PROFILE_MASK, &mask);
-        printf("OpenGL profile mask: %s (0x%08x)\n", get_profile_name(mask), mask);
+        printf("OpenGL profile mask (0x%08x): %s\n", mask, get_profile_name(mask));
 
         printf("OpenGL profile parsed by GLFW: %s\n",
                get_glfw_profile_name(glfwGetWindowParam(window, GLFW_OPENGL_PROFILE)));

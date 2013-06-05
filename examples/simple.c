ee91dea7418dbacf95b9eae7408de4f214993575
Camilla Berglund <elmindreda@elmindreda.org>
2013-06-05 21:55:21 +0200

    Added key input to quick tutorial.

--- examples/simple.c
+++ examples/simple.c
@@ -34,6 +34,12 @@ static void error_callback(int error, const char* description)
     fputs(description, stderr);
 }
 
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -51,6 +57,8 @@ int main(void)
     }
 
     glfwMakeContextCurrent(window);
+
+    glfwSetKeyCallback(window, key_callback);
 
     while (!glfwWindowShouldClose(window))
     {

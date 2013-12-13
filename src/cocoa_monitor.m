139803fd07cf461a69cfe0d3f7eebcf514807627
Matthew Henry <matt.henry@me.com>
2013-12-14 01:31:46 +1000

    Replace an input error call I accidentally removed

--- src/cocoa_monitor.m
+++ src/cocoa_monitor.m
@@ -123,7 +123,7 @@ static const char* getDisplayName(CGDirectDisplayID displayID)
     if (!names || !CFDictionaryGetValueIfPresent(names, CFSTR("en_US"),
                              (const void**) &value))
     {
-        
+        _glfwInputError(GLFW_PLATFORM_ERROR, "Failed to retrieve display name");
         CFRelease(info);
         return strdup("Unknown");
     }

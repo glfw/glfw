611006af185ffd35b99173d4ec2fe22d55d5b230
Camilla Berglund <elmindreda@elmindreda.org>
2014-01-22 01:32:00 +0100

    Updated API version in comment headers.

--- src/init.c
+++ src/init.c
@@ -1,5 +1,5 @@
 //========================================================================
-// GLFW 3.0 - www.glfw.org
+// GLFW 3.1 - www.glfw.org
 //------------------------------------------------------------------------
 // Copyright (c) 2002-2006 Marcus Geelnard
 // Copyright (c) 2006-2010 Camilla Berglund <elmindreda@elmindreda.org>

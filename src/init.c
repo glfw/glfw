bc219056a695a153299d3e964e5fed1d4af4c30b
Camilla Berglund <elmindreda@elmindreda.org>
2014-11-06 19:38:27 +0100

    Removed superfluous comments.

--- src/init.c
+++ src/init.c
@@ -153,11 +153,9 @@ GLFWAPI void glfwTerminate(void)
 
     memset(&_glfw.callbacks, 0, sizeof(_glfw.callbacks));
 
-    // Close all remaining windows
     while (_glfw.windowListHead)
         glfwDestroyWindow((GLFWwindow*) _glfw.windowListHead);
 
-    // Destroy all cursors
     while (_glfw.cursorListHead)
         glfwDestroyCursor((GLFWcursor*) _glfw.cursorListHead);
 

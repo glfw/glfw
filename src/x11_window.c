glfw/glfw

commit 4d146cb39dd2e08a7df7fea97d01c6f1a1d5a98e

Fixed hole in mouse button sequence on X11.

src/x11_window.c
@@ -1011,7 +1011,7 @@ static void processEvent(XEvent *event)
                 // Additional buttons after 7 are treated as regular buttons
                 // We subtract 4 to fill the gap left by scroll input above
                 _glfwInputMouseClick(window,
-                                     event->xbutton.button - 4,
+                                     event->xbutton.button - Button1 - 4,
                                      GLFW_PRESS,
                                      mods);
             }

commit c43f0829936203adf2e6d42d3697e43c47092a07

Fixed X11 mouse button mismatch.
Bug introduced by 4d146cb39d.

Closes #449.

src/x11_window.c
@@ -1049,7 +1049,7 @@ static void processEvent(XEvent *event)
                 // Additional buttons after 7 are treated as regular buttons
                 // We subtract 4 to fill the gap left by scroll input above
                 _glfwInputMouseClick(window,
-                                     event->xbutton.button - 4,
+                                     event->xbutton.button - Button1 - 4,
                                      GLFW_RELEASE,
                                      mods);
             }
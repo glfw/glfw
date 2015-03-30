d95b79f9eb50c14123d4476dbc8e7c089bdd92a6
Camilla Berglund <elmindreda@elmindreda.org>
2015-03-30 23:20:49 +0200

    Documentation grammar fix.

--- include/GLFW/glfw3.h
+++ include/GLFW/glfw3.h
@@ -531,7 +531,7 @@ extern "C" {
  *
  *  @par
  *  Some pre-installed Windows graphics drivers do not support OpenGL.  AMD only
- *  supports OpenGL ES via EGL, while Nvidia and Intel only supports it via
+ *  supports OpenGL ES via EGL, while Nvidia and Intel only support it via
  *  a WGL or GLX extension.  OS X does not provide OpenGL ES at all.  The Mesa
  *  EGL, OpenGL and OpenGL ES libraries do not interface with the Nvidia binary
  *  driver.

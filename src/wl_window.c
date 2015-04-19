glfw/glfw

commit e253beaa2b499b1972081cf9931e91ea21badd70

wayland: Implement glfwGetCursorPos
Closes #499.

src/wl_window.c
@@ -379,8 +379,10 @@ void _glfwPlatformPostEmptyEvent(void)
 
 void _glfwPlatformGetCursorPos(_GLFWwindow* window, double* xpos, double* ypos)
 {
-    // TODO
-    fprintf(stderr, "_glfwPlatformGetCursorPos not implemented yet\n");
+    if (xpos)
+        *xpos = window->wl.cursorX;
+    if (ypos)
+        *ypos = window->wl.cursorY;
 }
 
 void _glfwPlatformSetCursorPos(_GLFWwindow* window, double x, double y)

src/wl_init.c
@@ -86,6 +86,11 @@ static void pointerHandleMotion(void* data,
                         "Wayland: GLFW_CURSOR_DISABLED not supported");
         return;
     }
+    else
+    {
+        window->wl.cursorPosX = wl_fixed_to_double(sx);
+        window->wl.cursorPosY = wl_fixed_to_double(sy);
+    }
 
     _glfwInputCursorMotion(window,
                            wl_fixed_to_double(sx),

src/wl_platform.h
@@ -66,6 +66,7 @@ typedef struct _GLFWwindowWayland
     struct wl_shell_surface*    shell_surface;
     struct wl_callback*         callback;
     _GLFWcursor*                currentCursor;
+    double                      cursorPosX, cursorPosY;
 } _GLFWwindowWayland;
 
 

commit 90cd4b6e2e61d7297e888e4fe75547bb300ce108

Wayland: Fix typo introduced in e253beaa, preventing compilation

src/wl_window.c
@@ -380,9 +380,9 @@ void _glfwPlatformPostEmptyEvent(void)
 void _glfwPlatformGetCursorPos(_GLFWwindow* window, double* xpos, double* ypos)
 {
     if (xpos)
-        *xpos = window->wl.cursorX;
+        *xpos = window->wl.cursorPosX;
     if (ypos)
-        *ypos = window->wl.cursorY;
+        *ypos = window->wl.cursorPosY;
 }
 
 void _glfwPlatformSetCursorPos(_GLFWwindow* window, double x, double y)
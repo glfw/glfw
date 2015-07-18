adb74e0cb9d246fe62e726dd61bc53cc6580f6be
Yaron Cohen-Tal <yaronct@gmail.com>
2015-07-18 13:54:59 +0300

    Fixed CMake modules to find EGL, GLESv1 and GLESv2 on some Windows implementations.

--- CMake/modules/FindGLESv1.cmake
+++ CMake/modules/FindGLESv1.cmake
@@ -6,7 +6,7 @@
 
 find_path(GLESv1_INCLUDE_DIR NAMES GLES/gl.h PATHS /opt/vc/include)
 
-set(GLESv1_NAMES ${GLESv1_NAMES} GLESv1_CM)
+set(GLESv1_NAMES ${GLESv1_NAMES} GLESv1_CM libGLES_CM)
 find_library(GLESv1_LIBRARY NAMES ${GLESv1_NAMES} PATHS /opt/vc/lib)
 
 include(FindPackageHandleStandardArgs)

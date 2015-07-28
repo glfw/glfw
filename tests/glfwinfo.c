37e13361f535674f2d6551bcf89bad4203873b66
Camilla Berglund <elmindreda@elmindreda.org>
2015-07-28 14:51:36 +0200

    Fixed last manual inclusion of glext.h.

--- tests/glfwinfo.c
+++ tests/glfwinfo.c
@@ -23,8 +23,8 @@
 //
 //========================================================================
 
+#define GLFW_INCLUDE_GLEXT
 #include <GLFW/glfw3.h>
-#include <GL/glext.h>
 
 #include <stdio.h>
 #include <stdlib.h>

584d5f0f3148ebfc2a3cebdaf5726ee70c292930
Camilla Berglund <elmindreda@elmindreda.org>
2015-11-08 15:30:15 +0100

    Remove dependency on eglplatform.h header
    Fixes #636.

--- src/egl_context.h
+++ src/egl_context.h
@@ -39,7 +39,27 @@
  #define _glfw_dlsym(handle, name) dlsym(handle, name)
 #endif
 
-#include <EGL/eglplatform.h>
+#if defined(_GLFW_USE_EGLPLATFORM_H)
+ #include <EGL/eglplatform.h>
+#elif defined(_GLFW_WIN32)
+ #define EGLAPIENTRY __stdcall
+typedef HDC EGLNativeDisplayType;
+typedef HWND EGLNativeWindowType;
+#elif defined(_GLFW_X11)
+ #define EGLAPIENTRY
+typedef Display* EGLNativeDisplayType;
+typedef Window EGLNativeWindowType;
+#elif defined(_GLFW_WAYLAND)
+ #define EGLAPIENTRY
+typedef struct wl_display* EGLNativeDisplayType;
+typedef struct wl_egl_window* EGLNativeWindowType;
+#elif defined(_GLFW_MIR)
+ #define EGLAPIENTRY
+typedef MirEGLNativeDisplayType EGLNativeDisplayType;
+typedef MirEGLNativeWindowType EGLNativeWindowType;
+#else
+ #error "No supported EGL platform selected"
+#endif
 
 #define EGL_SUCCESS	0x3000
 #define EGL_NOT_INITIALIZED	0x3001
@@ -94,6 +114,7 @@
 #define EGL_NO_DISPLAY ((EGLDisplay) 0)
 #define EGL_NO_CONTEXT ((EGLContext) 0)
 
+typedef int EGLint;
 typedef unsigned int EGLBoolean;
 typedef unsigned int EGLenum;
 typedef void* EGLConfig;

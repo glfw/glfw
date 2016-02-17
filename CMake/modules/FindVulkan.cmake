d6975a708a6a3ec6b7e24746d92b32a04e555fe1
Camilla Berglund <elmindreda@elmindreda.org>
2016-02-17 08:26:45 +0100

    Fix search path for 32-bit Vulkan import library

--- CMake/modules/FindVulkan.cmake
+++ CMake/modules/FindVulkan.cmake
@@ -7,8 +7,13 @@
 if (WIN32)
     find_path(VULKAN_INCLUDE_DIR NAMES vulkan/vulkan.h HINTS
         "$ENV{VK_SDK_PATH}/Include")
-    find_library(VULKAN_LIBRARY NAMES vulkan-1 HINTS
-        "$ENV{VK_SDK_PATH}/Bin")
+    if (CMAKE_CL_64)
+        find_library(VULKAN_LIBRARY NAMES vulkan-1 HINTS
+            "$ENV{VK_SDK_PATH}/Bin")
+    else()
+        find_library(VULKAN_LIBRARY NAMES vulkan-1 HINTS
+            "$ENV{VK_SDK_PATH}/Bin32")
+    endif()
 else()
     find_path(VULKAN_INCLUDE_DIR NAMES vulkan/vulkan.h HINTS
         "$ENV{VULKAN_SDK}/include")

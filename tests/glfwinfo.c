eabc64fb7dd2a0a3006fd0d5079beb1a7c5f8daf
Camilla Berglund <elmindreda@elmindreda.org>
2016-02-17 08:06:38 +0100

    Disable unused prototypes

--- tests/glfwinfo.c
+++ tests/glfwinfo.c
@@ -23,8 +23,9 @@
 //
 //========================================================================
 
-#include <glad/glad.h>
+#define VK_NO_PROTOTYPES
 #include <vulkan/vulkan.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include <stdio.h>

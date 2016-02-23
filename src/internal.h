glfw/glfw

commit 71c72db1e35ae4ca7a0688e055aad3e4f193cb58

wayland: Pre-multiply custom cursor image alpha
Since the Wayland SHM buffer format is implicitly premultiplied and the
GLFWimage pixels are defined to be non-premultiplied, we need to
convert the non-premultiplied pixels to premultiplied when filling the
buffer.

Related to #707.

src/internal.h
@@ -233,6 +233,13 @@ typedef VkResult (APIENTRY * PFN_vkEnumerateInstanceExtensionProperties)(const c
         y = t;                    \
     }
 
+// Helper for non-premultiplied alpha to premultiplied alpha conversion
+static inline unsigned char _glfwMultiplyAlpha(unsigned char alpha,
+					       unsigned char value)
+{
+    return (unsigned char) ((value * (unsigned int) alpha) / 255);
+}
+
 
 //========================================================================
 // Platform-independent structures

src/wl_window.c
@@ -588,10 +588,12 @@ int _glfwPlatformCreateCursor(_GLFWcursor* cursor,
     unsigned char* target = data;
     for (i = 0;  i < image->width * image->height;  i++, source += 4)
     {
-        *target++ = source[2];
-        *target++ = source[1];
-        *target++ = source[0];
-        *target++ = source[3];
+        unsigned char alpha = source[3];
+
+        *target++ = _glfwMultiplyAlpha(alpha, source[2]);
+        *target++ = _glfwMultiplyAlpha(alpha, source[1]);
+        *target++ = _glfwMultiplyAlpha(alpha, source[0]);
+        *target++ = alpha;
     }
 
     cursor->wl.buffer =

commit 93b4c828c1a1f3703c4811bb126d4f4400ec53ca

Fix VC++ build failure
Regression caused by 71c72db1e35ae4ca7a0688e055aad3e4f193cb58.

src/internal.h
@@ -233,13 +233,6 @@ typedef VkResult (APIENTRY * PFN_vkEnumerateInstanceExtensionProperties)(const c
         y = t;                    \
     }
 
-// Helper for non-premultiplied alpha to premultiplied alpha conversion
-static inline unsigned char _glfwMultiplyAlpha(unsigned char alpha,
-					       unsigned char value)
-{
-    return (unsigned char) ((value * (unsigned int) alpha) / 255);
-}
-
 
 //========================================================================
 // Platform-independent structures

src/wl_window.c
@@ -588,12 +588,12 @@ int _glfwPlatformCreateCursor(_GLFWcursor* cursor,
     unsigned char* target = data;
     for (i = 0;  i < image->width * image->height;  i++, source += 4)
     {
-        unsigned char alpha = source[3];
+        unsigned int alpha = source[3];
 
-        *target++ = _glfwMultiplyAlpha(alpha, source[2]);
-        *target++ = _glfwMultiplyAlpha(alpha, source[1]);
-        *target++ = _glfwMultiplyAlpha(alpha, source[0]);
-        *target++ = alpha;
+        *target++ = (unsigned char) ((source[2] * alpha) / 255);
+        *target++ = (unsigned char) ((source[1] * alpha) / 255);
+        *target++ = (unsigned char) ((source[0] * alpha) / 255);
+        *target++ = (unsigned char) alpha;
     }
 
     cursor->wl.buffer =

src/x11_init.c
@@ -690,12 +690,12 @@ Cursor _glfwCreateCursorX11(const GLFWimage* image, int xhot, int yhot)
 
     for (i = 0;  i < image->width * image->height;  i++, target++, source += 4)
     {
-        unsigned char alpha = source[3];
+        unsigned int alpha = source[3];
 
         *target = (alpha << 24) |
-                  (_glfwMultiplyAlpha(alpha, source[0]) << 16) |
-                  (_glfwMultiplyAlpha(alpha, source[1]) <<  8) |
-                  _glfwMultiplyAlpha(alpha, source[2]);
+                  ((unsigned char) ((source[0] * alpha) / 255) << 16) |
+                  ((unsigned char) ((source[1] * alpha) / 255) <<  8) |
+                  ((unsigned char) ((source[2] * alpha) / 255) <<  0);
     }
 
     cursor = XcursorImageLoadCursor(_glfw.x11.display, native);
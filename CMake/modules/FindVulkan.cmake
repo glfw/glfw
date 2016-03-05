3c9142f37ec38189677f9a368e8ee1b775c97aae
Camilla Berglund <elmindreda@elmindreda.org>
2016-03-05 23:56:40 +0100

    Add check for VULKAN_SDK envvar on Windows
    The LunarG SDK will be standardizing on this name.

--- CMake/modules/FindVulkan.cmake
+++ CMake/modules/FindVulkan.cmake
@@ -6,12 +6,15 @@
 
 if (WIN32)
     find_path(VULKAN_INCLUDE_DIR NAMES vulkan/vulkan.h HINTS
+        "$ENV{VULKAN_SDK}/Include"
         "$ENV{VK_SDK_PATH}/Include")
     if (CMAKE_CL_64)
         find_library(VULKAN_LIBRARY NAMES vulkan-1 HINTS
+            "$ENV{VULKAN_SDK}/Bin"
             "$ENV{VK_SDK_PATH}/Bin")
     else()
         find_library(VULKAN_LIBRARY NAMES vulkan-1 HINTS
+            "$ENV{VULKAN_SDK}/Bin32"
             "$ENV{VK_SDK_PATH}/Bin32")
     endif()
 else()

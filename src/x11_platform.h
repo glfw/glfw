4ff66a7818e3eaa5362c828a18220b12f1cd9bc8
Camilla Löwy <elmindreda@glfw.org>
2017-03-27 01:19:41 +0200

    X11: Fix IM-duplicated key events leaking through
    Fixes #747.
    Fixes #964.

--- src/x11_platform.h
+++ src/x11_platform.h
@@ -141,8 +141,7 @@ typedef struct _GLFWwindowX11
     // The last position the cursor was warped to by GLFW
     int             warpCursorPosX, warpCursorPosY;
 
-    // The information from the last KeyPress event
-    unsigned int    lastKeyCode;
+    // The time of the last KeyPress event
     Time            lastKeyTime;
 
 } _GLFWwindowX11;

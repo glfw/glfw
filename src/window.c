glfw/glfw

commit 72ac5badb0a188b0d912b1e4fa79a5e143253474

Add GLFW_CENTER_CURSOR
Adds a hint for controlling whether the cursor is centered over newly
created full screen windows.

Fixes #749.
Closes #842.

src/window.c
@@ -345,6 +345,9 @@ GLFWAPI void glfwWindowHint(int hint, int value)
         case GLFW_COCOA_FRAME_AUTOSAVE:
             _glfw.hints.window.ns.frame = value ? GLFW_TRUE : GLFW_FALSE;
             break;
+	case GLFW_CENTER_CURSOR:
+	    _glfw.hints.window.centerCursor = value ? GLFW_TRUE : GLFW_FALSE;
+	    break;
         case GLFW_CLIENT_API:
             _glfw.hints.context.client = value;
             break;

docs/window.dox
@@ -428,6 +428,7 @@ GLFW_FOCUSED                  | `GLFW_TRUE`                 | `GLFW_TRUE` or `GL
 GLFW_AUTO_ICONIFY             | `GLFW_TRUE`                 | `GLFW_TRUE` or `GLFW_FALSE`
 GLFW_FLOATING                 | `GLFW_FALSE`                | `GLFW_TRUE` or `GLFW_FALSE`
 GLFW_MAXIMIZED                | `GLFW_FALSE`                | `GLFW_TRUE` or `GLFW_FALSE`
+GLFW_CENTER_CURSOR            | `GLFW_TRUE`                 | `GLFW_TRUE` or `GLFW_FALSE`
 GLFW_RED_BITS                 | 8                           | 0 to `INT_MAX` or `GLFW_DONT_CARE`
 GLFW_GREEN_BITS               | 8                           | 0 to `INT_MAX` or `GLFW_DONT_CARE`
 GLFW_BLUE_BITS                | 8                           | 0 to `INT_MAX` or `GLFW_DONT_CARE`
@@ -1040,6 +1041,10 @@ __GLFW_MAXIMIZED__ indicates whether the specified window is maximized.  See
 @anchor GLFW_VISIBLE_attrib
 __GLFW_VISIBLE__ indicates whether the specified window is visible.  See @ref
 window_hide for details.
+
+`GLFW_CENTER_CURSOR` indicates whether the cursor should be moved to the center
+of the full screen window during creation. This is ignored for windowed mode
+windows.
 
 @anchor GLFW_RESIZABLE_attrib
 __GLFW_RESIZABLE__ indicates whether the specified window is resizable _by the

include/GLFW/glfw3.h
@@ -686,6 +686,7 @@ extern "C" {
  *  [window attribute](@ref GLFW_MAXIMIZED_attrib).
  */
 #define GLFW_MAXIMIZED              0x00020008
+#define GLFW_CENTER_CURSOR          0x00020009
 
 /*! @brief Framebuffer bit depth hint.
  *

src/cocoa_window.m
@@ -1096,7 +1096,8 @@ int _glfwPlatformCreateWindow(_GLFWwindow* window,
         if (!acquireMonitor(window))
             return GLFW_FALSE;
 
-        centerCursor(window);
+    if (wndconfig->centerCursor)
+            centerCursor(window);
     }
 
     return GLFW_TRUE;

src/internal.h
@@ -277,6 +277,7 @@ struct _GLFWwndconfig
     GLFWbool      autoIconify;
     GLFWbool      floating;
     GLFWbool      maximized;
+    GLFWbool      centerCursor;
     struct {
         GLFWbool  retina;
         GLFWbool  frame;

src/win32_window.c
@@ -1037,7 +1037,10 @@ int _glfwPlatformCreateWindow(_GLFWwindow* window,
         if (!acquireMonitor(window))
             return GLFW_FALSE;
 
-        centerCursor(window);
+        if (wndconfig->centerCursor)
+        {
+            centerCursor(window);
+        }
     }
 
     return GLFW_TRUE;

src/x11_window.c
@@ -1597,7 +1597,10 @@ int _glfwPlatformCreateWindow(_GLFWwindow* window,
         if (!acquireMonitor(window))
             return GLFW_FALSE;
 
-        centerCursor(window);
+        if(wndconfig->centerCursor)
+        {
+            centerCursor(window);
+        }
     }
 
     XFlush(_glfw.x11.display);

commit a1154247fad6ebd1e109e4cdbc32b93266646835

Fix default value of GLFW_CENTER_CURSOR
Regression introduced by 72ac5badb0a188b0d912b1e4fa79a5e143253474.

Fixes #1105.

src/window.c
@@ -246,11 +246,12 @@ void glfwDefaultWindowHints(void)
 
     // The default is a focused, visible, resizable window with decorations
     memset(&_glfw.hints.window, 0, sizeof(_glfw.hints.window));
-    _glfw.hints.window.resizable   = GLFW_TRUE;
-    _glfw.hints.window.visible     = GLFW_TRUE;
-    _glfw.hints.window.decorated   = GLFW_TRUE;
-    _glfw.hints.window.focused     = GLFW_TRUE;
-    _glfw.hints.window.autoIconify = GLFW_TRUE;
+    _glfw.hints.window.resizable    = GLFW_TRUE;
+    _glfw.hints.window.visible      = GLFW_TRUE;
+    _glfw.hints.window.decorated    = GLFW_TRUE;
+    _glfw.hints.window.focused      = GLFW_TRUE;
+    _glfw.hints.window.autoIconify  = GLFW_TRUE;
+    _glfw.hints.window.centerCursor = GLFW_TRUE;
 
     // The default is 24 bits of color, 24 bits of depth and 8 bits of stencil,
     // double buffered
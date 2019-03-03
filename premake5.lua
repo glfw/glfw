83c5a685a237a5472346a7ad6fc4dc507dc1fd0f
Leonardo Marinovic <Leonardo.leo.201@gmail.com>
2019-03-03 14:28:47 +0100

    Update premake5 file with added Windows support

--- premake5.lua
+++ premake5.lua
@@ -18,10 +18,35 @@ project "GLFW"
         "src/window.c"
     }
 
+    filter "system:windows"
+        buildoptions { "-std=c11", "-lgdi32" }
+        systemversion "10.0.17134.0"
+        staticruntime "On"
+        
+        files
+        {
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
+        }
+
+        defines 
+        { 
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
+        }
+
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
+
     filter "system:linux"
-
         pic "On"
-
         systemversion "latest"
         staticruntime "On"
         
@@ -43,29 +68,3 @@ project "GLFW"
         {
             "_GLFW_X11"
         }
-    
-    filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
-        systemversion "latest"
-        staticruntime "On"
-        
-        files
-        {
-            "src/win32_init.c",
-            "src/win32_joystick.c",
-            "src/win32_monitor.c",
-            "src/win32_time.c",
-            "src/win32_thread.c",
-            "src/win32_window.c",
-            "src/wgl_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c"
-        }
-
-        defines 
-        { 
-            "_GLFW_WIN32",
-            "_CRT_SECURE_NO_WARNINGS"
-        }
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"

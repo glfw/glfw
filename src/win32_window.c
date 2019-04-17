cbd0e93d0f78ac9614ec21f450d31484b4e8273e
Luiz Monclar <lggmonclar@gmail.com>
2019-04-17 01:51:24 -0300

    Replace 'GetActiveWindow()' calls with 'GetForegroundWindow()' for the win32 platform

--- src/win32_window.c
+++ src/win32_window.c
@@ -1788,7 +1788,7 @@ void _glfwPlatformSetWindowMonitor(_GLFWwindow* window,
 
 int _glfwPlatformWindowFocused(_GLFWwindow* window)
 {
-    return window->win32.handle == GetActiveWindow();
+    return window->win32.handle == GetForegroundWindow();
 }
 
 int _glfwPlatformWindowIconified(_GLFWwindow* window)
@@ -1918,7 +1918,7 @@ void _glfwPlatformPollEvents(void)
         }
     }
 
-    handle = GetActiveWindow();
+    handle = GetForegroundWindow();
     if (handle)
     {
         // NOTE: Shift keys on Windows tend to "stick" when both are pressed as

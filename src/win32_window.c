cb3bc2dd674f70ae1c9d17e587063973f13a4134
A. Tombs <lazylumpster+github@gmail.com>
2019-05-28 01:31:31 +0100

    Remove _glfwInputWindowMonitor from win32_window.c

--- src/win32_window.c
+++ src/win32_window.c
@@ -1723,7 +1723,7 @@ void _glfwPlatformSetWindowMonitor(_GLFWwindow* window,
     if (window->monitor)
         releaseMonitor(window);
 
-    _glfwInputWindowMonitor(window, monitor);
+    window->monitor = monitor;
 
     if (window->monitor)
     {

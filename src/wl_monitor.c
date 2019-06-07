e463e85bbab1bd1c805f9a7d733ccd9caa5fa45f
Luflosi <luflosi@luflosi.de>
2019-06-08 00:06:22 +0200

    Fix typo
    Closes #1513.

--- src/wl_monitor.c
+++ src/wl_monitor.c
@@ -200,7 +200,7 @@ void _glfwPlatformGetVideoMode(_GLFWmonitor* monitor, GLFWvidmode* mode)
 GLFWbool _glfwPlatformGetGammaRamp(_GLFWmonitor* monitor, GLFWgammaramp* ramp)
 {
     _glfwInputError(GLFW_PLATFORM_ERROR,
-                    "Wayland: Gamma ramp access it not available");
+                    "Wayland: Gamma ramp access is not available");
     return GLFW_FALSE;
 }
 

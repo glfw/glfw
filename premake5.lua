d7dbc1890ac1c05d806940d36ef7feb2295d54a7
Georgi Mirazchiyski <georgimweb@gmail.com>
2019-10-06 14:24:27 +0100

    Update outputdir to OutputDir

--- premake5.lua
+++ premake5.lua
@@ -2,8 +2,8 @@ project "GLFW"
 	kind "StaticLib"
 	language "C"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. OutputDir .. "/%{prj.name}")
+	objdir ("bin-int/" .. OutputDir .. "/%{prj.name}")
 
 	files
 	{

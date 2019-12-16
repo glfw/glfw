4e70c95aa3bcb666320e17f2097f33d18a7fc225
Camilla Löwy <elmindreda@elmindreda.org>
2019-12-16 17:44:17 +0100

    X11: Cleanup
    The window state action is not an atom.

--- src/x11_window.c
+++ src/x11_window.c
@@ -2619,7 +2619,7 @@ void _glfwPlatformSetWindowFloating(_GLFWwindow* window, GLFWbool enabled)
 
     if (_glfwPlatformWindowVisible(window))
     {
-        const Atom action = enabled ? _NET_WM_STATE_ADD : _NET_WM_STATE_REMOVE;
+        const long action = enabled ? _NET_WM_STATE_ADD : _NET_WM_STATE_REMOVE;
         sendEventToWM(window,
                       _glfw.x11.NET_WM_STATE,
                       action,

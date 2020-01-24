0c27ed1d0ef36672f0ef28d86bf8e108cc6e379d
Camilla Löwy <elmindreda@elmindreda.org>
2020-01-24 09:34:54 +0100

    X11: Fix setting the clipboard string to itself

--- src/x11_window.c
+++ src/x11_window.c
@@ -2997,8 +2997,9 @@ void _glfwPlatformSetCursor(_GLFWwindow* window, _GLFWcursor* cursor)
 
 void _glfwPlatformSetClipboardString(const char* string)
 {
+    char* copy = _glfw_strdup(string);
     free(_glfw.x11.clipboardString);
-    _glfw.x11.clipboardString = _glfw_strdup(string);
+    _glfw.x11.clipboardString = copy;
 
     XSetSelectionOwner(_glfw.x11.display,
                        _glfw.x11.CLIPBOARD,

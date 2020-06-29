glfw/glfw

commit ad9eb768c9518dbe94232e5201d318d512ad9beb

Cocoa: Fix non-BMP Unicode codepoint input
Supplimentary Plane codepoints were reported as UTF-16 surrogate pairs.

Fixes #1635.

src/cocoa_window.m
@@ -731,14 +731,24 @@ static const NSRange kEmptyRange = { NSNotFound, 0 };
     else
         characters = (NSString*) string;
 
-    const NSUInteger length = [characters length];
-    for (NSUInteger i = 0;  i < length;  i++)
+    NSRange range = NSMakeRange(0, [characters length]);
+    while (range.length)
     {
-        const unichar codepoint = [characters characterAtIndex:i];
-        if ((codepoint & 0xff00) == 0xf700)
-            continue;
+        uint32_t codepoint = 0;
 
-        _glfwInputChar(window, codepoint, mods, plain);
+        if ([characters getBytes:&codepoint
+                       maxLength:sizeof(codepoint)
+                      usedLength:NULL
+                        encoding:NSUTF32StringEncoding
+                         options:0
+                           range:range
+                  remainingRange:&range])
+        {
+            if ((codepoint & 0xff00) == 0xf700)
+                continue;
+
+            _glfwInputChar(window, codepoint, mods, plain);
+        }
     }
 }
 

commit e47832ffcf924a0580ce79431b9601f0ca0915ee

Cocoa: Fix regression in private use area check
This fixes a regression introduced by
ad9eb768c9518dbe94232e5201d318d512ad9beb.

src/cocoa_window.m
@@ -744,7 +744,7 @@ static const NSRange kEmptyRange = { NSNotFound, 0 };
                            range:range
                   remainingRange:&range])
         {
-            if ((codepoint & 0xff00) == 0xf700)
+            if (codepoint >= 0xf700 && codepoint <= 0xf7ff)
                 continue;
 
             _glfwInputChar(window, codepoint, mods, plain);
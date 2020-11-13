8ab920ea67987ea68ac6000ae8785e54b6ad7483
hdf89shfdfs <juanr0911@gmail.com>
2020-11-13 07:36:41 -0500

    Created a more generalized helper module
    I think this will help glfw's cmake build system be more readable and adaptable to changes to cmake.

--- CMake/glfw.cmake
+++ CMake/glfw.cmake
@@ -1,7 +1,13 @@
-if (DEFINED glfw_msvc_runtime_library_include_guard)
+# This cmake module is meant to hold helper functions/macros
+# that make maintaining the cmake build system much easier.
+# This is especially helpful since glfw needs to provide coverage
+# for multiple versions of cmake.
+#
+# Any functions/macros should have a glfw_* prefix to avoid problems
+if (DEFINED glfw_include_guard)
     return()
 endif()
-set(glfw_msvc_runtime_library_include_guard ON)
+set(glfw_include_guard ON)
 
 include(CMakeDependentOption)
 
@@ -53,3 +59,25 @@ macro(glfw_msvc_runtime_library GLFW_STANDALONE)
         endif()
     endif()
 endmacro()
+
+# Despite the fact that glfw is using a range to cover newer projects
+# cmake_minimum_required(VERSION 3.1...3.X FATAL_ERROR)
+# This still doesn't cover all cmake versions since this range functionality was only added
+# in cmake 3.12, so explicitly setting these policies allow for more projects to work properly
+# in between cmake 3.1 -> 3.11
+macro(glfw_set_new_policies)
+    if (${CMAKE_VERSION} VERSION_LESS "3.12")
+        if (POLICY CMP0054)
+            cmake_policy(SET CMP0054 NEW)
+        endif()
+
+        if (POLICY CMP0069)
+            cmake_policy(SET CMP0069 NEW)
+        endif()
+
+        if (POLICY CMP0077)
+            cmake_policy(SET CMP0077 NEW)
+        endif()
+    endif()
+endmacro()
+

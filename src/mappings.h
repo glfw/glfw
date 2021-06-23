74a8ba26c3b9bb830462ffbebefa133c38368fa3
Camilla Löwy <elmindreda@elmindreda.org>
2021-06-23 18:53:06 +0200

    Simplify parsing of default gamepad mappings
    The outer glfwUpdateGamepadMappings function is now bypassed when
    parsing the default gamepad mappings.  The data in mappings.h does not
    contain any comments or line breaks.  Also this is done before joystick
    support has been initialized, so there is no need to look for matching
    devices.
    
    Finally, the array of default mappings is pre-allocated.  This has no
    measurable performance impact but does generate a lot of calls, which
    won't be nice for a user provided custom allocator to deal with.
    
    (cherry picked from commit 201400b974b63eb7f23eb7d8563589df9c699d7c)

--- src/mappings.h
+++ src/mappings.h
@@ -997,7 +997,5 @@ const char* _glfwDefaultMappings[] =
 "03000000120c0000100e000011010000,ZEROPLUS P4 Gamepad,a:b1,b:b2,back:b8,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,dpup:h0.1,guide:b12,leftshoulder:b4,leftstick:b10,lefttrigger:a3,leftx:a0,lefty:a1,rightshoulder:b5,rightstick:b11,righttrigger:a4,rightx:a2,righty:a5,start:b9,x:b0,y:b3,platform:Linux,",
 "03000000120c0000101e000011010000,ZEROPLUS P4 Wired Gamepad,a:b1,b:b2,back:b8,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,dpup:h0.1,guide:b12,leftshoulder:b4,leftstick:b10,lefttrigger:a3,leftx:a0,lefty:a1,rightshoulder:b5,rightstick:b11,righttrigger:a4,rightx:a2,righty:a5,start:b9,x:b0,y:b3,platform:Linux,",
 #endif // GLFW_BUILD_LINUX_MAPPINGS
-
-NULL
 };
 

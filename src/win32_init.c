1d7339f3fc10a7a7b8c6b13346e65a54f4e105af
Richard Boldiš <richard@boldis.dev>
2021-08-24 02:30:41 +0200

    Win32: Fix invalid preprocessor command 'warning'
    The GCCism #warning had snuck into the Win32 platform code.
    
    Merges #1953.
    
    (cherry picked from commit 2ac7e20aafebcd309b33c7b696e50e42971909d6)

--- src/win32_init.c
+++ src/win32_init.c
@@ -40,7 +40,7 @@ static const GUID _glfw_GUID_DEVINTERFACE_HID =
 #if defined(_GLFW_USE_HYBRID_HPG) || defined(_GLFW_USE_OPTIMUS_HPG)
 
 #if defined(_GLFW_BUILD_DLL)
- #warning "These symbols must be exported by the executable and have no effect in a DLL"
+ #pragma message("These symbols must be exported by the executable and have no effect in a DLL")
 #endif
 
 // Executables (but not DLLs) exporting this symbol with this value will be

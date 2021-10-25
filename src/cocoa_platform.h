f75c251deccde745d3d3f313aa81955095369d6f
Josh Codd <jjc21@live.co.uk>
2021-10-26 00:14:30 +0100

    Cocoa: Fix deprecation of kIOMasterPortDefault
    This adds a workaround for kIOMasterPortDefault having been deprecated
    in favor of kIOMainPortDefault in macOS 12.0.
    
    Closes #1980

--- src/cocoa_platform.h
+++ src/cocoa_platform.h
@@ -44,6 +44,13 @@ typedef void* id;
 // NOTE: Many Cocoa enum values have been renamed and we need to build across
 //       SDK versions where one is unavailable or the other deprecated
 //       We use the newer names in code and these macros to handle compatibility
+#if MAC_OS_X_VERSION_MAX_ALLOWED < 120000
+ #define kIOMainPortDefault kIOMasterPortDefault
+#endif
+
+// NOTE: Many Cocoa enum values have been renamed and we need to build across
+//       SDK versions where one is unavailable or the other deprecated
+//       We use the newer names in code and these macros to handle compatibility
 #if MAC_OS_X_VERSION_MAX_ALLOWED < 101400
  #define NSOpenGLContextParameterSwapInterval NSOpenGLCPSwapInterval
  #define NSOpenGLContextParameterSurfaceOpacity NSOpenGLCPSurfaceOpacity

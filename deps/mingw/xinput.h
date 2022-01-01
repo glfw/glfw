4353b622517ff55f3b4e85cef8654613efeff003
Snowiiii <71594357+Snowiiii@users.noreply.github.com>
2022-01-01 22:13:22 +0100

    Update xinput.h

--- deps/mingw/xinput.h
+++ deps/mingw/xinput.h
@@ -110,14 +110,17 @@
  * which are being used.
  */
 
-#define XINPUT_DEVTYPE_GAMEPAD          0x01
-#define XINPUT_DEVSUBTYPE_GAMEPAD       0x01
-#define XINPUT_DEVSUBTYPE_WHEEL         0x02
-#define XINPUT_DEVSUBTYPE_ARCADE_STICK  0x03
-#define XINPUT_DEVSUBTYPE_FLIGHT_SICK   0x04
-#define XINPUT_DEVSUBTYPE_DANCE_PAD     0x05
-#define XINPUT_DEVSUBTYPE_GUITAR        0x06
-#define XINPUT_DEVSUBTYPE_DRUM_KIT      0x08
+#define XINPUT_DEVTYPE_GAMEPAD             0x01
+#define XINPUT_DEVSUBTYPE_GAMEPAD          0x01
+#define XINPUT_DEVSUBTYPE_WHEEL            0x02
+#define XINPUT_DEVSUBTYPE_ARCADE_STICK     0x03
+#define XINPUT_DEVSUBTYPE_FLIGHT_SICK      0x04
+#define XINPUT_DEVSUBTYPE_DANCE_PAD        0x05
+#define XINPUT_DEVSUBTYPE_GUITAR           0x06
+#define XINPUT_DEVSUBTYPE_GUITAR_ALTERNATE 0x07
+#define XINPUT_DEVSUBTYPE_DRUM_KIT         0x08
+#define XINPUT_DEVSUBTYPE_GUITAR_BASS      0x0b
+#define XINPUT_DEVSUBTYPE_ARCADE_PAD       0x13
 
 /*
  * These are used with the XInputGetCapabilities function to
@@ -155,6 +158,11 @@
 
 #define XUSER_MAX_COUNT                 4
 #define XUSER_INDEX_ANY                 0x000000FF
+
+#define XINPUT_CAPS_FFB_SUPPORTED       0x0001
+#define XINPUT_CAPS_WIRELESS            0x0002
+#define XINPUT_CAPS_PMD_SUPPORTED       0x0008
+#define XINPUT_CAPS_NO_NAVIGATION       0x0010
 
 /*
  * Defines the structure of an xbox 360 joystick.
@@ -231,6 +239,8 @@ DWORD WINAPI XInputGetKeystroke(DWORD, DWORD, PXINPUT_KEYSTROKE);
 DWORD WINAPI XInputGetCapabilities(DWORD, DWORD, XINPUT_CAPABILITIES*);
 DWORD WINAPI XInputGetDSoundAudioDeviceGuids(DWORD, GUID*, GUID*);
 DWORD WINAPI XInputGetBatteryInformation(DWORD, BYTE, XINPUT_BATTERY_INFORMATION*);
+
+DWORD WINAPI XInputGetStateEx(DWORD, XINPUT_STATE*);
 
 #ifdef __cplusplus
 }

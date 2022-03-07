367d06deafb00057850796abc02489756a7c6c18
Camilla Löwy <elmindreda@elmindreda.org>
2022-03-07 19:19:31 +0100

    Win32: Fix scale fixup losing initial position
    The window content scale correction at creation overwrote the inital,
    more pleasant placement of the window by CW_USEDEFAULT, if the window
    was created with GLFW_MAXIMIZED set.  This is because the translation
    to screen coordinates was done using the current position, not the
    position from the restored window rect.

--- src/win32_window.c
+++ src/win32_window.c
@@ -1316,9 +1316,6 @@ static int createNativeWindow(_GLFWwindow* window,
             }
         }
 
-        ClientToScreen(window->win32.handle, (POINT*) &rect.left);
-        ClientToScreen(window->win32.handle, (POINT*) &rect.right);
-
         if (_glfwIsWindows10AnniversaryUpdateOrGreaterWin32())
         {
             AdjustWindowRectExForDpi(&rect, style, FALSE, exStyle,
@@ -1328,6 +1325,10 @@ static int createNativeWindow(_GLFWwindow* window,
             AdjustWindowRectEx(&rect, style, FALSE, exStyle);
 
         GetWindowPlacement(window->win32.handle, &wp);
+        OffsetRect(&rect,
+                   wp.rcNormalPosition.left - rect.left,
+                   wp.rcNormalPosition.top - rect.top);
+
         wp.rcNormalPosition = rect;
         wp.showCmd = SW_HIDE;
         SetWindowPlacement(window->win32.handle, &wp);

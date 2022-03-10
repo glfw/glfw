fad44068e58bece8a029ac8161072be7e43856b4
daipom <fukuda@clear-code.com>
2022-03-10 17:55:08 +0900

    Apply shibukawa's fix of GLFW for Windows
    This fix is based on shibukawa's fix:
      https://github.com/glfw/glfw/pull/658
      https://github.com/shibukawa/glfw-1/commit/d36a164423c933948661f3f17576e5a6388ff251
    
    Some minor coding style changes are made, but not yet follow glfw's one,
    and some comments doesn't follow recent changes. So further work is
    needed.

--- src/internal.h
+++ src/internal.h
@@ -551,6 +551,15 @@ struct _GLFWwindow
     double              virtualCursorPosX, virtualCursorPosY;
     GLFWbool            rawMouseMotion;
 
+    // Preedit texts
+    unsigned int*       preeditText;
+    int                 ntext;
+    int                 ctext;
+    int*                preeditAttributeBlocks;
+    int                 nblocks;
+    int                 cblocks;
+    int                 preeditCursorPosX, preeditCursorPosY, preeditCursorHeight;
+
     _GLFWcontext        context;
 
     struct {
@@ -570,6 +579,8 @@ struct _GLFWwindow
         GLFWkeyfun                key;
         GLFWcharfun               character;
         GLFWcharmodsfun           charmods;
+        GLFWpreeditfun            preedit;
+        GLFWimestatusfun          imestatus;
         GLFWdropfun               drop;
     } callbacks;
 
@@ -920,6 +931,8 @@ void _glfwInputKey(_GLFWwindow* window,
                    int key, int scancode, int action, int mods);
 void _glfwInputChar(_GLFWwindow* window,
                     uint32_t codepoint, int mods, GLFWbool plain);
+void _glfwInputPreedit(_GLFWwindow* window, int focusedBlock);
+void _glfwInputIMEStatus(_GLFWwindow* window);
 void _glfwInputScroll(_GLFWwindow* window, double xoffset, double yoffset);
 void _glfwInputMouseClick(_GLFWwindow* window, int button, int action, int mods);
 void _glfwInputCursorPos(_GLFWwindow* window, double xpos, double ypos);
@@ -940,6 +953,9 @@ void _glfwInputError(int code, const char* format, ...)
 void _glfwInputError(int code, const char* format, ...);
 #endif
 
+void _glfwPlatformResetPreeditText(_GLFWwindow* window);
+void _glfwPlatformSetIMEStatus(_GLFWwindow* window, int active);
+int  _glfwPlatformGetIMEStatus(_GLFWwindow* window);
 
 //////////////////////////////////////////////////////////////////////////
 //////                       GLFW internal API                      //////

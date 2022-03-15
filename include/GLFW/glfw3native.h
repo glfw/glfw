c2188e2d0aab7c97de7b749e131521df5b4f5ddb
Camilla Löwy <elmindreda@elmindreda.org>
2022-03-15 17:59:57 +0100

    Fix accidental C99 in C89 header
    (cherry picked from commit add0521efb1fc0e95082716748b7aad837753fba)

--- include/GLFW/glfw3native.h
+++ include/GLFW/glfw3native.h
@@ -82,9 +82,10 @@ extern "C" {
  *************************************************************************/
 
 #if defined(GLFW_EXPOSE_NATIVE_WIN32) || defined(GLFW_EXPOSE_NATIVE_WGL)
- // This is a workaround for the fact that glfw3.h needs to export APIENTRY (for
- // example to allow applications to correctly declare a GL_KHR_debug callback)
- // but windows.h assumes no one will define APIENTRY before it does
+ /* This is a workaround for the fact that glfw3.h needs to export APIENTRY (for
+  * example to allow applications to correctly declare a GL_KHR_debug callback)
+  * but windows.h assumes no one will define APIENTRY before it does
+  */
  #if defined(GLFW_APIENTRY_DEFINED)
   #undef APIENTRY
   #undef GLFW_APIENTRY_DEFINED

7de1c5c10a6340ff46dc774bccf9e8091c6662b8
Doug Binks <doug@enkisoftware.com>
2022-06-09 13:55:33 +0100

    Minor formating fix

--- src/cocoa_init.m
+++ src/cocoa_init.m
@@ -550,7 +550,7 @@ GLFWbool _glfwConnectCocoa(int platformID, _GLFWplatform* platform)
         _glfwWaitEventsCocoa,
         _glfwWaitEventsTimeoutCocoa,
         _glfwPostEmptyEventCocoa,
-		_glfwCreateUserContextCocoa,
+        _glfwCreateUserContextCocoa,
         _glfwGetEGLPlatformCocoa,
         _glfwGetEGLNativeDisplayCocoa,
         _glfwGetEGLNativeWindowCocoa,

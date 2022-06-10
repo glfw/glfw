040712ce99ea787a85ae68b75fa452ee817274b8
Camilla Löwy <elmindreda@elmindreda.org>
2022-06-10 18:53:22 +0200

    Wayland: Cleanup

--- src/wl_window.c
+++ src/wl_window.c
@@ -401,7 +401,8 @@ static void surfaceHandleLeave(void* userData,
     checkScaleChange(window);
 }
 
-static const struct wl_surface_listener surfaceListener = {
+static const struct wl_surface_listener surfaceListener =
+{
     surfaceHandleEnter,
     surfaceHandleLeave
 };
@@ -557,7 +558,8 @@ static void xdgSurfaceHandleConfigure(void* userData,
     }
 }
 
-static const struct xdg_surface_listener xdgSurfaceListener = {
+static const struct xdg_surface_listener xdgSurfaceListener =
+{
     xdgSurfaceHandleConfigure
 };
 

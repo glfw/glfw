afb127769a5a39246ae80e2489f0d4cf43a32fbb
Camilla Löwy <elmindreda@elmindreda.org>
2022-06-10 18:36:16 +0200

    Wayland: Add tracking of XDG fullscreen state

--- src/wl_platform.h
+++ src/wl_platform.h
@@ -234,6 +234,7 @@ typedef struct _GLFWwindowWayland
     GLFWbool                    visible;
     GLFWbool                    maximized;
     GLFWbool                    activated;
+    GLFWbool                    fullscreen;
     GLFWbool                    hovered;
     GLFWbool                    transparent;
     struct wl_surface*          surface;
--- src/wl_window.c
+++ src/wl_window.c
@@ -522,8 +522,9 @@ static void xdgToplevelHandleConfigure(void* userData,
     else if (maximized && !window->wl.maximized)
         _glfwInputWindowMaximize(window, GLFW_TRUE);
 
-    window->wl.activated = activated;
-    window->wl.maximized = maximized;
+    window->wl.activated  = activated;
+    window->wl.maximized  = maximized;
+    window->wl.fullscreen = fullscreen;
 }
 
 static void xdgToplevelHandleClose(void* userData,

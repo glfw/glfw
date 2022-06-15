e79d6fe40a83c897323ab48367dd5b26a9120353
Camilla Löwy <elmindreda@elmindreda.org>
2022-06-15 17:57:54 +0200

    Wayland: Fix events emitted before ack_configure
    Window iconfication and maximization events were being emitted before
    xdg_surface::configure, making it possible for user code to indirectly
    commit surface changes from those event callbacks before
    xdg_surface::ack_configure.
    
    This postpones those events until after the ack has been sent.
    
    (cherry picked from commit 85f5a51912c3329137377cd2ba0f0a7a34887ed7)

--- src/wl_platform.h
+++ src/wl_platform.h
@@ -189,6 +189,10 @@ typedef struct _GLFWwindowWayland
 
     struct {
         int                     width, height;
+        GLFWbool                maximized;
+        GLFWbool                iconified;
+        GLFWbool                activated;
+        GLFWbool                fullscreen;
     } pending;
 
     struct {

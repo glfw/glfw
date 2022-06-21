1ac6fbc49999bb80820ad6906e71a1b7d6a61a8f
Camilla Löwy <elmindreda@elmindreda.org>
2022-06-21 17:37:37 +0200

    Wayland: Remove premature surface commit calls
    Note that the handling of configure events, acks and commits is still
    not ideal.  This is just a small step in, hopefully, a good direction.
    
    Fullscreen toggling via glfwSetWindowMonitor now works on Weston, but
    mostly incidentally.
    
    (cherry picked from commit be7f4513c05a45e6252f940408e139741a462986)

--- src/wl_window.c
+++ src/wl_window.c
@@ -339,7 +339,6 @@ static void setContentAreaOpaque(_GLFWwindow* window)
 
     wl_region_add(region, 0, 0, window->wl.width, window->wl.height);
     wl_surface_set_opaque_region(window->wl.surface, region);
-    wl_surface_commit(window->wl.surface);
     wl_region_destroy(region);
 }
 
@@ -2296,7 +2295,6 @@ void _glfwPlatformSetCursorPos(_GLFWwindow* window, double x, double y)
         zwp_locked_pointer_v1_set_cursor_position_hint(
             window->wl.pointerLock.lockedPointer,
             wl_fixed_from_double(x), wl_fixed_from_double(y));
-        wl_surface_commit(window->wl.surface);
     }
 }
 

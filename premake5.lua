99d40b082bb0576f11c288bfa3457fef453aef41
Nikhil Bharadwaj <44049892+pixelbrahma@users.noreply.github.com>
2022-07-23 19:20:40 +0100

    Premake changed for Linux and git ignore updated

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,6 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -29,8 +28,35 @@ project "GLFW"
 		"src/null_joystick.c"
 	}
 
+	filter "system:linux"
+		pic "On"
+
+		systemversion "latest"
+		staticruntime "On"
+
+		files
+		{
+			"src/x11_init.c",
+			"src/x11_monitor.c",
+			"src/x11_window.c",
+			"src/xkb_unicode.c",
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/glx_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c",
+			"src/linux_joystick.c"
+		}
+
+		defines
+		{
+			"_GLFW_X11"
+		}
+
+
 	filter "system:windows"
 		systemversion "latest"
+		staticruntime "On"
 
 		files
 		{

3a60992a418aad88717db32353bec22e8bb7dab3
Camilla Löwy <elmindreda@glfw.org>
2022-07-25 16:30:11 +0200

    Wayland: Add error for unsupported feature

--- src/wl_window.c
+++ src/wl_window.c
@@ -2252,6 +2252,8 @@ void _glfwGetCursorPosWayland(_GLFWwindow* window, double* xpos, double* ypos)
 
 void _glfwSetCursorPosWayland(_GLFWwindow* window, double x, double y)
 {
+    _glfwInputError(GLFW_FEATURE_UNAVAILABLE,
+                    "Wayland: The platform does not support setting the cursor position");
 }
 
 void _glfwSetCursorModeWayland(_GLFWwindow* window, int mode)

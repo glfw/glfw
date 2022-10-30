d59fba270620240047682973ed7de54db90522b6
Bozhidar Ivanov <ivanov6@abv.bg>
2022-10-31 00:03:47 +0200

    Fix update GLFW compilation.

--- premake5.lua
+++ premake5.lua
@@ -9,14 +9,27 @@ project "glfw"
 	{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
+		
+        "src/internal.h",
+		"src/platform.h",
+		"src/mappings.h",
+        "src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
+		"src/platform.c",
+		"src/vulkan.c",
 		"src/window.c",
-		"src/vulkan.c"
+		"src/egl_context.c",
+		"src/osmesa_context.c",
+		"src/null_platform.h",
+		"src/null_joystick.h",
+		"src/null_init.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c"
 	}
+
 	filter "system:linux"
 		pic "On"
 
@@ -31,10 +44,10 @@ project "glfw"
 			"src/xkb_unicode.c",
 			"src/posix_time.c",
 			"src/posix_thread.c",
+			"src/posix_module.c",
 			"src/glx_context.c",
-			"src/egl_context.c",
 			"src/osmesa_context.c",
-			"src/linux_joystick.c",
+			"src/linux_joystick.c"
 		}
 
 		defines
@@ -49,15 +62,16 @@ project "glfw"
 
 		files
 		{
+			"src/win32_platform.h",
+			"src/win32_joystick.h",
 			"src/win32_init.c",
 			"src/win32_joystick.c",
 			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
 			"src/win32_window.c",
 			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
+			"src/win32_module.c",
+            "src/win32_time.c",
+			"src/win32_thread.c"
 		}
 
 		defines 

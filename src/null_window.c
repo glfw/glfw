1f532f8f3fb5c2c86304ac4b63528ad79bb9e3f4
Daijiro Fukuda <fukuda@clear-code.com>
2022-12-15 12:01:14 +0900

    Win32: Support IME
    This commit re-organizes 9d9af132610829f295c34ceb81b17af8b567b76f.
    
    * Use dynamic load for Imm32.
    * Generalize platform-specific features to _GLFWplatform.
    * Add caret-position info to preedit-callback.
    * Add cursorWidth to preeditCursor and related APIs.
    * Handle UTF16 data correctly.
    * Handle GCS_RESULTSTR so that committed texts are processed correctly.
    * Handle WM_IME_ENDCOMPOSITION to clear preedit.
    * Handle WM_IME_SETCONTEXT.
        * https://learn.microsoft.com/en-us/windows/win32/intl/wm-ime-setcontext#remarks
    * Refactor code shapes and variable names.
    
    Co-authored-by: Takuro Ashie <ashie@clear-code.com>

--- src/null_window.c
+++ src/null_window.c
@@ -551,6 +551,23 @@ const char* _glfwGetClipboardStringNull(void)
     return _glfw.null.clipboardString;
 }
 
+void _glfwUpdatePreeditCursorRectangleNull(_GLFWwindow* window)
+{
+}
+
+void _glfwResetPreeditTextNull(_GLFWwindow* window)
+{
+}
+
+void _glfwSetIMEStatusNull(_GLFWwindow* window, int active)
+{
+}
+
+int _glfwGetIMEStatusNull(_GLFWwindow* window)
+{
+    return GLFW_FALSE;
+}
+
 EGLenum _glfwGetEGLPlatformNull(EGLint** attribs)
 {
     if (_glfw.egl.EXT_platform_base && _glfw.egl.MESA_platform_surfaceless)

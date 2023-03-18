3fada28a8f016003d043f1ee983ab7fec72c2470
jumailj <jumailj.work@gmail.com>
2023-03-18 12:17:15 +0530

    change configurations for release and debug

--- premake5.lua
+++ premake5.lua
@@ -55,9 +55,11 @@ project "GLFW"
 		}
 
 	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
+		defines "SANDBOX_DEBUG"
+		buildoptions "/MT"	--it uses multithread, static version of the run-time library.[MDd]
+		symbols "On" -- it allows us to debug code. line by line. which the exe contain suppoting elements to debug.
 
 	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+		defines "SANDBOX_RELEASE"
+		buildoptions "/MT"	--it uses multithread, static version of the run-time library.[MDd]
+		optimize "On" -- remove all the debuging datas. 

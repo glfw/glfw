66c769f5c47b6229bca80278c8acbc281842cecb
Revoke <maksim_25_03@ukr.net>
2023-11-05 00:16:25 +0200

    GLWF primake fix

--- premake5.lua
+++ premake5.lua
@@ -9,13 +9,7 @@ project "GLFW"
 	{
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
-        "src/glfw_config.h",
-        "src/context.c",
-        "src/init.c",
-        "src/input.c",
-        "src/monitor.c",
-        "src/vulkan.c",
-        "src/window.c"
+        "src/*.c",
     }
     
 	filter "system:windows"
@@ -23,18 +17,7 @@ project "GLFW"
         systemversion "latest"
         staticruntime "On"
         
-        files
-        {
-            "src/win32_init.c",
-            "src/win32_joystick.c",
-            "src/win32_monitor.c",
-            "src/win32_time.c",
-            "src/win32_thread.c",
-            "src/win32_window.c",
-            "src/wgl_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c"
-        }
+     
 
 		defines 
 		{ 

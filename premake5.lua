99babbe93a48a1bb9e66094e10dec6b01cce6be0
Xander Cole <65436489+Clackroe@users.noreply.github.com>
2023-11-07 16:16:40 -0500

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,74 +1,96 @@
 project "GLFW"
-	kind "StaticLib"
-	language "C"
+kind "StaticLib"
+language "C"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
-	{
-		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-		"src/vulkan.c",
-		"src/window.c"
-	}
-	filter "system:linux"
-		pic "On"
+files
+{
+    "include/GLFW/glfw3.h",
+    "include/GLFW/glfw3native.h",
+    "src/internal.h",
+    "src/platform.h",
+    "src/mappings.h",
+    "src/context.c",
+    "src/init.c",
+    "src/input.c",
+    "src/monitor.c",
+    "src/platform.c",
+    "src/vulkan.c",
+    "src/window.c",
+    "src/egl_context.c",
+    "src/osmesa_context.c",
+    "src/null_platform.h",
+    "src/null_joystick.h",
+    "src/null_init.c",
 
-		systemversion "latest"
-		staticruntime "On"
+    "src/null_monitor.c",
+    "src/null_window.c",
+    "src/null_joystick.c",
 
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-		}
+}
+filter "system:linux"
+    pic "On"
 
-		defines
-		{
-			"_GLFW_X11"
-		}
+    systemversion "latest"
+    staticruntime "On"
 
-	filter "system:windows"
-		systemversion "latest"
-		staticruntime "On"
+    files
+    {
+        "src/x11_init.c",
+        "src/x11_monitor.c",
+        "src/x11_window.c",
+        "src/xkb_unicode.c",
+        "src/posix_time.c",
+        "src/posix_thread.c",
+        "src/glx_context.c",
+        "src/egl_context.c",
+        "src/osmesa_context.c",
+        "src/linux_joystick.c"
+    }
 
-		files
-		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
-		}
+    defines
+    {
+        "_GLFW_X11"
+        
+    }
 
-		defines 
-		{ 
-			"_GLFW_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
-		}
+filter "system:windows"
+    systemversion "latest"
+    staticruntime "On"
+    
+    -- buildoptions{
+    --     "/MT"
+    -- }
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
+    files
+    {
+        "src/win32_init.c",
+        "src/win32_module.c",
+        "src/win32_joystick.c",
+        "src/win32_monitor.c",
+        "src/win32_time.h",
+        "src/win32_time.c",
+        "src/win32_thread.h",
+        "src/win32_thread.c",
+        "src/win32_window.c",
+        "src/wgl_context.c",
+        "src/egl_context.c",
+        "src/osmesa_context.c"
+    }
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+    defines 
+    { 
+        "_GLFW_WIN32",
+        "_CRT_SECURE_NO_WARNINGS"
+
+    }
+
+filter "configurations:Debug"
+    runtime "Debug"
+    symbols "On"
+
+filter "configurations:Release"
+    runtime "Release"
+    optimize "On"

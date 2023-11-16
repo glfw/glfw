2a4dd9c28f663c1ef410d6430aaa3fe90085bf6c
Camilla Löwy <elmindreda@elmindreda.org>
2023-11-16 23:08:30 +0100

    Cleanup

--- src/linux_joystick.h
+++ src/linux_joystick.h
@@ -50,7 +50,7 @@ typedef struct _GLFWlibraryLinux
     int                     inotify;
     int                     watch;
     regex_t                 regex;
-    GLFWbool                regex_compiled;
+    GLFWbool                regexCompiled;
     GLFWbool                dropped;
 } _GLFWlibraryLinux;
 

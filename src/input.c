5f936a6276490c1e82d8de7a733f8148d4acf659
Camilla Löwy <elmindreda@elmindreda.org>
2023-12-04 18:48:24 +0100

    Fix glfwGetKeyName not checking key token is valid
    (cherry picked from commit b1517e5b3ee6f060f2e7ebd47c9de0c10e6ad411)

--- src/input.c
+++ src/input.c
@@ -616,6 +616,12 @@ GLFWAPI const char* glfwGetKeyName(int key, int scancode)
 
     if (key != GLFW_KEY_UNKNOWN)
     {
+        if (key < GLFW_KEY_SPACE || key > GLFW_KEY_LAST)
+        {
+            _glfwInputError(GLFW_INVALID_ENUM, "Invalid key %i", key);
+            return NULL;
+        }
+
         if (key != GLFW_KEY_KP_EQUAL &&
             (key < GLFW_KEY_KP_0 || key > GLFW_KEY_KP_ADD) &&
             (key < GLFW_KEY_APOSTROPHE || key > GLFW_KEY_WORLD_2))

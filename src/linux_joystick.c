glfw/glfw

commit 7c7cc59889883fa0c19daee122373271670e3b26

Linux: Fix memory leak when inotify init failed
This introduces regex_compiled boolean to track whether the regex is
compiled successfully.

Closes #2229

(cherry picked from commit c992226a9c63f80e157d6bd9bf2372acd2a1c795)

src/linux_joystick.c
@@ -283,7 +283,9 @@ GLFWbool _glfwInitJoysticksLinux(void)
 
     // Continue without device connection notifications if inotify fails
 
-    if (regcomp(&_glfw.linjs.regex, "^event[0-9]\\+$", 0) != 0)
+    _glfw.linjs.regex_compiled = regcomp(&_glfw.linjs.regex,
+                                         "^event[0-9]\\+$", 0) == 0;
+    if (!_glfw.linjs.regex_compiled)
     {
         _glfwInputError(GLFW_PLATFORM_ERROR, "Linux: Failed to compile regex");
         return GLFW_FALSE;
@@ -342,6 +344,9 @@ void _glfwTerminateJoysticksLinux(void)
 
         close(_glfw.linjs.inotify);
     }
+
+    if (_glfw.linjs.regex_compiled)
+        regfree(&_glfw.linjs.regex);
 }
 
 void _glfwDetectJoystickConnectionLinux(void)

src/linux_joystick.h
@@ -53,6 +53,7 @@ typedef struct _GLFWlibraryLinux
     int                     inotify;
     int                     watch;
     regex_t                 regex;
+    GLFWbool                regex_compiled;
     GLFWbool                dropped;
 } _GLFWlibraryLinux;
 

commit 7998629ad256823b1b487486691793db070aa9a0

Linux: Fix regfree being called on invalid data
This was caused by a careless merge that did not take the differences
between the branches into account.

Regression introduced by 7c7cc59889883fa0c19daee122373271670e3b26.

Fixes #2464

src/linux_joystick.c
@@ -334,8 +334,6 @@ void _glfwTerminateJoysticksLinux(void)
             closeJoystick(js);
     }
 
-    regfree(&_glfw.linjs.regex);
-
     if (_glfw.linjs.inotify > 0)
     {
         if (_glfw.linjs.watch > 0)
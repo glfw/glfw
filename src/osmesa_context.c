15b771759d7d9cf834656d0efa35c3990d692fc2
Camilla Löwy <elmindreda@elmindreda.org>
2024-01-24 15:32:32 +0100

    OSMesa: Fix headers included before shared header
    The shared header, internal.h, must be the first header included in
    every compilation unit of GLFW.
    
    (cherry picked from commit 291f4d89cd3019b5d0be4fd5d97699036d1b0a67)

--- src/osmesa_context.c
+++ src/osmesa_context.c
@@ -27,12 +27,11 @@
 // Please use C89 style variable declarations in this file because VS 2010
 //========================================================================
 
+#include "internal.h"
+
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-
-#include "internal.h"
-
 
 static void makeContextCurrentOSMesa(_GLFWwindow* window)
 {

b394a01c54ce070c938e275d8651a633ba474ded
khunguraharman <khungura_harman@hotmail.com>
2024-02-11 22:56:43 -0800

    Reformatted

--- premake5.lua
+++ premake5.lua
@@ -1,19 +1,22 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-
+		
 		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 		objdir ("obj/" .. outputdir .. "/%{prj.name}")
 
-		files{
-		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/monitor.c",
-		"src/vulkan.c",
-		"src/window.c"
+		test = "bin/" .. outputdir .. "/%{prj.name}"
+		print(test)
+		files
+		{
+			"include/GLFW/glfw3.h",
+			"include/GLFW/glfw3native.h",
+			"src/glfw_config.h",
+			"src/context.c",
+			"src/init.c",
+			"src/monitor.c",
+			"src/vulkan.c",
+			"src/window.c"
 		}
 
 		filter "system:windows"
@@ -21,16 +24,17 @@ project "GLFW"
 		systemversion "latest"
 		staticruntime "On"
 
-		files{
-		"src/win32_init.c",
-		"src/win32_joystick.c",
-		"src/win32_monitor.c",
-		"src/win32_time.c",
-		"src/win32_thread.c",
-		"src/win32_window.c",
-		"src/wgl_context.c",
-		"src/egl_context.c",
-		"src/osmesa_context.c"
+		files
+		{
+			"src/win32_init.c",
+			"src/win32_joystick.c",
+			"src/win32_monitor.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
 		}
 
 		defines{

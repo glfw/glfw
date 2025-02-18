7fb289808fff34f7ca2de2de141d49e5c0ff14a2
TuxTheAstronaut <tuxtheastronaut@protonmail.com>
2025-02-18 02:06:43 -0500

    Wayland: Ignore cursor position errors from spamming the console

--- src/wl_window.c
+++ src/wl_window.c
@@ -2832,8 +2832,9 @@ void _glfwGetCursorPosWayland(_GLFWwindow* window, double* xpos, double* ypos)
 
 void _glfwSetCursorPosWayland(_GLFWwindow* window, double x, double y)
 {
-    _glfwInputError(GLFW_FEATURE_UNAVAILABLE,
-                    "Wayland: The platform does not support setting the cursor position");
+    // Ignore This Error from spamming
+    //_glfwInputError(GLFW_FEATURE_UNAVAILABLE,
+    //                "Wayland: The platform does not support setting the cursor position");
 }
 
 void _glfwSetCursorModeWayland(_GLFWwindow* window, int mode)

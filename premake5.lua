0b77dbffc54a140b2330707a5a1dcfaf61b73fe6
ljc1320551158@163.com <ljc1320551158@163.com>
2025-03-06 02:57:33 +0800

    premake configuration file

---
+++ premake5.lua
@@ -0,0 +1,60 @@
+project "GLFW"
+    kind "StaticLib"
+    language "C"
+ 
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+ 
+	files
+	{
+        "include/GLFW/glfw3.h",
+        "include/GLFW/glfw3native.h",
+        "src/glfw_config.h",       --？？should it to be delete?
+        "src/internal.h",       --
+        "src/platform.h",       --
+        "src/mappings.h",       --
+        "src/context.c",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
+        "src/platform.c",       --
+        "src/vulkan.c",
+        "src/window.c",
+        "src/egl_context.c",        --
+        "src/null_platform.h",      --
+        "src/null_joystick.h",      --
+        "src/null_init.c",      --
+    
+        "src/null_monitor.c",       --
+        "src/null_window.c",        --
+        "src/null_joystick.c",      --
+    }
+ 
+	filter "system:windows"
+        buildoptions { "-std=c11", "-lgdi32" }
+        systemversion "latest"
+        staticruntime "On"
+ 
+        files
+        {
+            "src/win32_init.c",
+            "src/win32_module.c",   --
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.h",     --
+            "src/win32_time.c",
+            "src/win32_thread.h",   --解决Vulkan.obj相关问题
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
+        }
+ 
+		defines 
+		{ 
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
+		}
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
